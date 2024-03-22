6e2788b004473c8f38f63b70357fa628affd180d
Rashmina-Senadheera <163754024+YashmiAruksha@users.noreply.github.com>
2024-03-22 23:27:42 +0530

    committing sri lankan map

--- front-end/weather-app/src/components/map.jsx
+++ front-end/weather-app/src/components/map.jsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import "../style.css";
+import {MapContainer, Marker, Polygon} from "react-leaflet";
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { districtData } from "../lk";
+
 
 const Map = () => {
   const navigate = useNavigate();
@@ -24,6 +30,48 @@ const Map = () => {
     getUserDetails(accessToken);
   }, [navigate]);
 
+  const markers = [
+    {
+      geocode: [7.4698, 80.6217],
+      popup: "Matale",
+    },
+    {
+      geocode: [6.93194, 79.84778],
+      popup: "Colombo",
+    },
+    {
+      geocode: [7.29754, 81.68202],
+      popup: "Ampara",
+    },
+  ];
+  const sunny = new Icon({
+    iconUrl: require("../img/sunny.png"),
+    iconSize: [24, 24],
+  });
+
+  const rain = new Icon({
+    iconUrl: require("../img/rain.png"),
+    iconSize: [24, 24],
+  });
+
+  const storm = new Icon({
+    iconUrl: require("../img/storm.png"),
+    iconSize: [24, 24],
+  });
+  const [showPopup, setShowPopup] = useState(false);
+  const [name, setName] = useState("hello");
+  const [temp, setTemp] = useState("hello");
+  const [humidity, setHumidity] = useState("hello");
+  const [weather, setWeather] = useState("sunny");
+  const colombo = [6.88, 79.861244];
+  const anuradhapura = [8.31223, 80.41306];
+  const togglePopup = (name) => {
+    setName(name);
+    setShowPopup(!showPopup);
+
+    // console.log(element);
+  };
+
   return (
     <>
       {userDetails ? (
@@ -45,6 +93,65 @@ const Map = () => {
           <h1>Loading...</h1>
         </div>
       )}
+    <MapContainer center={[7.8774222, 80.7003428]} zoom={7.5}>
+      {showPopup && (
+        <div className="popup" id="popup">
+          <div className="popup-inner">
+            <h2>{name}</h2>
+            <ul>
+              <li>Tempertature: </li>
+              <li>Humidity: </li>
+              <li>Name: </li>
+            </ul>
+            <button onClick={togglePopup}>Close Popup</button>
+          </div>
+        </div>
+      )}
+      <Marker position={colombo} icon={rain}></Marker>
+      <Marker position={anuradhapura} icon={sunny}></Marker>
+      {districtData.features.map((state) => {
+        let coordinates;
+        if (state.geometry.type === "Polygon") {
+          // For a single Polygon
+          coordinates = state.geometry.coordinates[0].map((item) => [
+            item[1],
+            item[0],
+          ]);
+        } else if (state.geometry.type === "MultiPolygon") {
+          // For MultiPolygon, handle each polygon separately
+          coordinates = state.geometry.coordinates.map((polygon) => {
+            return polygon[0].map((item) => [item[1], item[0]]);
+          });
+        }
+        const names = state.properties.name;
+        console.log(names);
+
+        return (
+          <Polygon
+            pathOptions={{
+              fillColor: "green",
+              fillOpacity: 0.7,
+              weight: 2,
+              opacity: 1,
+              dashArray: "3",
+              color: "white",
+            }}
+            positions={coordinates}
+            eventHandlers={{
+              mouseover: (e) => {
+                
+              },
+              mouseout: (e) => {
+                
+              },
+              click: (e) => {
+                
+              },
+            }}
+          />
+        );
+      })}
+    </MapContainer>
     </>
   );
 }
