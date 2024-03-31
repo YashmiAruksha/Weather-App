a1c67a45f8b00e0b53eda99c40bb8b769f408487
Yashmi <COBSCCOMP4Y222P-071@student.nibm.lk>
2024-03-31 23:48:30 +0530

    Add CORS to backend

--- back-end/server.js
+++ back-end/server.js
@@ -9,6 +9,14 @@ const { insertWeatherData } = require('./dataGenerator');
 
 const app = express();
 
+//CORS 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Middleware
 app.use(bodyParser.json());
 
