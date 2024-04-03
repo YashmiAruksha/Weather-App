2098e873ef4b1a4cad99b68f955a661eeed53cf1
Yashmi <COBSCCOMP4Y222P-071@student.nibm.lk>
2024-04-03 23:29:42 +0530

    Update package.jsom

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import Cookies from "js-cookie";
 import "../style.css";
 import {MapContainer, Marker, Polygon, Tooltip} from "react-leaflet";
